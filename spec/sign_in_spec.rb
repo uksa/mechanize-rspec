d1ff823d5df096942ba9de513acb598847009c83
Sam Treweek <samtreweek@gmail.com>
2015-03-24 20:53:20 +0000

    incorrect tag name

--- spec/sign_in_spec.rb
+++ spec/sign_in_spec.rb
@@ -4,7 +4,7 @@ require 'sign_in'
 
 RSpec.describe SignIn, '#users' do
 
-  H1_TAG = '.container>h3'
+  H3_TAG = '.container>h3'
 
   before(:each) do
     @url = 'http://www.uksa-testing.co.uk'
@@ -18,22 +18,22 @@ RSpec.describe SignIn, '#users' do
 
     it 'authentication fails if credentials are Unknown' do
       page = @sign_in.user(User.unknown_user)
-      expect(page.at(H1_TAG).text).to eq ('Sign in')
+      expect(page.at(H3_TAG).text).to eq ('Sign in')
     end
 
     it 'authentication logs in when credentials are Admin' do
       page = @sign_in.user(User.admin_user)
-      expect(page.at(H1_TAG).text).to eq ('Member Details')
+      expect(page.at(H3_TAG).text).to eq ('Member Details')
     end
 
     it 'authentication logs in when credentials are Standard User' do
       page = @sign_in.user(User.standard_user)
-      expect(page.at(H1_TAG).text).to eq ('Member Details')
+      expect(page.at(H3_TAG).text).to eq ('Member Details')
     end
 
     it 'authentication logs in when credentials are Suspend User' do
       page = @sign_in.user(User.suspended_user)
-      expect(page.at(H1_TAG).text).to eq ('Member Details')
+      expect(page.at(H3_TAG).text).to eq ('Member Details')
     end
 
   end
